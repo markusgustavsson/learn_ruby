c4246311876a9b2fea5c92d5b5d40a74b5dba4f2
markusgustavsson <markusgustavsson21@gmail.com>
2016-07-17 23:00:19 +0100

    simon says complete

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+
+def echo(a)
+	return a
+end
+
+def shout(a)
+	a.upcase
+end
+
+def repeat(a, b=2)
+	("#{a} "*b).chomp(" ")
+end
+
+def start_of_word(a,b)
+	return	a[0,b]
+end
+
+def first_word(a)
+	word=a.split(' ')
+	word[0].to_s
+end
+
+def titleize(x)
+  little_words = ["end", "over", "and", "the"]
+  x.capitalize.gsub(/(\w+)/) do |word|
+    little_words.include?(word) ? word : word.capitalize
+    end
+end
+
