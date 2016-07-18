63dbd8344289edda328ef06c757890cb02d881ff
markusgustavsson <markusgustavsson21@gmail.com>
2016-07-18 22:49:47 +0100

    did pig latin until two words

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+def translate(s)
+vowels = ['a','e','i','o','u']
+if s.include? (" ")
+
+
+if vowels.include? s[0]
+	s << "ay"
+elsif vowels.include? s[1]
+	s = s[1,s.length]+s[0]+"ay"
+elsif vowels.include? s[2]
+	s = s[2,s.length]+s[0]+s[1]+"ay"
+	end
+end
